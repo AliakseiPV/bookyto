0a89d4b77ffbaa167ede47d47ec059853a782482
Aliaksei Poliak <aliakseipv@gmail.com>
2023-06-20 06:50:48 -0400

    setting up store page

--- client/src/pages/StorePage/StorePage.jsx
+++ client/src/pages/StorePage/StorePage.jsx
@@ -3,7 +3,20 @@ import React from 'react';
 const StorePage = () => {
     return (
         <div>
-            Store
+            <div>
+                <div></div>
+                <div>Trending books</div>
+            </div>
+            <div>
+                <h1>Browse Genres</h1>
+                <div>
+                    <div>Search Bar</div>
+                    <div>
+                        SideBar
+                    </div>
+                    <div>Store</div>
+                </div>
+            </div>
         </div>
     );
 };
